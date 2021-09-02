c489277fbd957f5a2b3dc82b65ec9e29a8a6585d
lloam <2196143404@qq.com>
2021-09-02 19:51:18 +0800

    :art:剑指offer35复杂链表的复制

--- Sword_finger_offer/src/main/java/com/mao/剑指offer35复杂链表的复制/Solution.java
+++ Sword_finger_offer/src/main/java/com/mao/剑指offer35复杂链表的复制/Solution.java
@@ -42,42 +42,114 @@ public class Solution {
 //        return map.get(head);
 //    }
 
+//    /**
+//     * 还有一种想法就是在原节点的下一个节点创建新节点
+//     * 然后原节点.next = 原节点.next.next
+//     * 原节点.random = 原节点.next.random
+//     * @param head
+//     * @return
+//     */
+//    public Node copyRandomList(Node head) {
+//        if(head == null) return null;
+//        // 先创建出节点
+//        Node cur = head;
+//        while (cur != null) {
+//            Node temp = new Node(cur.val);
+//            temp.next = cur.next;
+//            cur.next = temp;
+//            cur = temp.next;
+//        }
+//        // 新节点的 random 指向
+//        cur = head;
+//        while (cur != null) {
+//            if(cur.random != null) {
+//                cur.next.random = cur.random.next;
+//            }
+//            cur = cur.next.next;
+//        }
+//        // 将原链表与新链表分开
+//        Node pre = head;
+//        cur = pre.next;
+//        Node res = cur;
+//        while (cur.next != null) {
+//            pre.next = cur.next;
+//            cur.next = cur.next.next;
+//            pre = pre.next;
+//            cur = cur.next;
+//        }
+//        // 还原原节点
+//        pre.next = null;
+//        return res;
+//    }
+
+//    /**
+//     *
+//     * @param head
+//     * @return
+//     */
+//    public Node copyRandomList(Node head) {
+//
+//        Node cur = head;
+//
+//        // 使用 map 集合将各节点点存储起来，并建立新的节点
+//        Map<Node,Node> map = new HashMap<Node, Node>();
+//
+//        while (cur != null) {
+//            map.put(cur, new Node(cur.val));
+//            cur = cur.next;
+//        }
+//
+//        cur = head;
+//
+//        while (cur != null) {
+//            map.get(cur).next = map.get(cur.next);
+//            map.get(cur).random = map.get(cur.random);
+//            cur = cur.next;
+//        }
+//
+//        return map.get(head);
+//    }
+
     /**
-     * 还有一种想法就是在原节点的下一个节点创建新节点
-     * 然后原节点.next = 原节点.next.next
-     * 原节点.random = 原节点.next.random
+     *
      * @param head
      * @return
      */
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-        // 先创建出节点
+
+        if (head == null) return null;
+
+        // 先创建出新的节点，在原节点的下一个节点创建新节点
         Node cur = head;
+        Node temp;
         while (cur != null) {
-            Node temp = new Node(cur.val);
+            temp = new Node(cur.val);
             temp.next = cur.next;
             cur.next = temp;
             cur = temp.next;
         }
-        // 新节点的 random 指向
+
+        // 然后直接新节点的 random 节点
         cur = head;
         while (cur != null) {
-            if(cur.random != null) {
+            if (cur.random != null) {
                 cur.next.random = cur.random.next;
             }
             cur = cur.next.next;
         }
-        // 将原链表与新链表分开
+
+        // 接着分离新链表与旧链表
         Node pre = head;
         cur = pre.next;
         Node res = cur;
-        while (pre.next != null) {
+        while (cur.next != null) {
             pre.next = cur.next;
             cur.next = cur.next.next;
             pre = pre.next;
             cur = cur.next;
         }
-        // 还原原节点
+
+        // 还原链表
         pre.next = null;
         return res;
     }
