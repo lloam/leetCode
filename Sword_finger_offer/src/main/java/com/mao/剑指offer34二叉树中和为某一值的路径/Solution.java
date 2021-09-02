ed455c7fda766526873d94a747dd6e707d62b69d
lloam <2196143404@qq.com>
2021-09-02 18:15:41 +0800

    :art:剑指offer34二叉树中和为某一值的路径

--- Sword_finger_offer/src/main/java/com/mao/剑指offer34二叉树中和为某一值的路径/Solution.java
+++ Sword_finger_offer/src/main/java/com/mao/剑指offer34二叉树中和为某一值的路径/Solution.java
@@ -12,36 +12,75 @@ import java.util.List;
  * 从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
  */
 public class Solution {
-    List<List<Integer>> allPath = new LinkedList<>();
-    LinkedList<Integer> path = new LinkedList<>();
-    public List<List<Integer>> pathSum(TreeNode root, int target) {
-        recur(root,target);
-        return allPath;
-    }
+//    List<List<Integer>> allPath = new LinkedList<>();
+//    LinkedList<Integer> path = new LinkedList<>();
+//    public List<List<Integer>> pathSum(TreeNode root, int target) {
+//        recur(root,target);
+//        return allPath;
+//    }
+//
+//    /**
+//     * 核心是递归，二叉树的先序遍历（根，左，右）和路径记录
+//     * @param root
+//     * @param target
+//     * @return
+//     */
+//    private void recur(TreeNode root, int target) {
+//        // 递归的结束条件
+//        if(root == null) return;
+//        // 将节点的值加入路径
+//        path.add(root.val);
+//        // target要减去节点值
+//        target -= root.val;
+//        // 如果当target 值为 0 时，而且root没有子节点，
+//        // 也就是root是叶子结点的时候target == 0，那么此路径就是符合的
+//        if(target == 0 && root.left == null && root.right == null){
+//            allPath.add(new LinkedList<>(path));
+//        }
+//        // 继续递归遍历子节点
+//        // 先遍历左节点，再遍历右节点，因为是先序遍历
+//        recur(root.left,target);
+//        recur(root.right,target);
+//        // 每次遍历完一条路线，就要一次删除路径中的值，方便父节点的兄弟节点的遍历
+//        path.removeLast();
+//    }
 
+    // 全局变量
+    List<List<Integer>> allPath = new LinkedList<List<Integer>>();
+    LinkedList<Integer> path = new LinkedList<Integer>();
     /**
-     * 核心是递归，二叉树的先序遍历（根，左，右）和路径记录
+     * 可以使用递归依次向下遍历
      * @param root
      * @param target
      * @return
      */
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        recur(root, target);
+        return allPath;
+    }
+
+    /**
+     * 递归每一个节点，将节点的值加入 path
+     * 如果 target == 0，将 path 加入 allPath
+     * list 清空
+     * @param root
+     * @param target
+     */
     private void recur(TreeNode root, int target) {
-        // 递归的结束条件
-        if(root == null) return;
-        // 将节点的值加入路径
+
+        if (root == null) return;
+
         path.add(root.val);
-        // target要减去节点值
+
         target -= root.val;
-        // 如果当target 值为 0 时，而且root没有子节点，
-        // 也就是root是叶子结点的时候target == 0，那么此路径就是符合的
-        if(target == 0 && root.left == null && root.right == null){
-            allPath.add(new LinkedList<>(path));
+
+        if (root.left == null && root.right == null && target == 0) {
+            allPath.add(new LinkedList<Integer>(path));
         }
-        // 继续递归遍历子节点
-        // 先遍历左节点，再遍历右节点，因为是先序遍历
-        recur(root.left,target);
-        recur(root.right,target);
-        // 每次遍历完一条路线，就要一次删除路径中的值，方便父节点的兄弟节点的遍历
+        recur(root.left, target);
+        recur(root.right, target);
+
+        // 遍历完当前节点和左右节点，要将路径中的左右节点清空
         path.removeLast();
     }
 }
