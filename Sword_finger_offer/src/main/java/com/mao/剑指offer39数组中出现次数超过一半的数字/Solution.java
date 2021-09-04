b1060a922de957c0bd53a4c6fc3b03bb33f73d68
lloam <2196143404@qq.com>
2021-09-04 23:21:46 +0800

    :art:剑指offer39数组中出现次数超过一半的数字

--- Sword_finger_offer/src/main/java/com/mao/剑指offer39数组中出现次数超过一半的数字/Solution.java
+++ Sword_finger_offer/src/main/java/com/mao/剑指offer39数组中出现次数超过一半的数字/Solution.java
@@ -8,25 +8,48 @@ package com.mao.剑指offer39数组中出现次数超过一半的数字;
  */
 public class Solution {
 
+//    /**
+//     * 求众数
+//     * 假设第一个数 x 是众数，那么记票数 votes = 1
+//     * 如果遇到第二个数仍然 = x，那么票数 votes++
+//     * 否则 votes--
+//     * 当 votes = 0的时候，当前数字设为众数
+//     * @param nums
+//     * @return
+//     */
+//    public int majorityElement(int[] nums) {
+//        // 假设第一个为众数
+//        int votes = 0, x = 0;
+//        for(int i = 1; i < nums.length; i++) {
+//            // 如果票数为 0 ，则当前数字为众数
+//            if(votes == 0) x = nums[i];
+//            if(nums[i] == x) {
+//                votes++;
+//            }else {
+//                votes--;
+//            }
+//        }
+//        return x;
+//    }
+
     /**
      * 求众数
-     * 假设第一个数 x 是众数，那么记票数 votes = 1
-     * 如果遇到第二个数仍然 = x，那么票数 votes++
-     * 否则 votes--
-     * 当 votes = 0的时候，当前数字设为众数
+     * 维护两个变量
+     * 一个是票数，票数最多的就是众数 vote
+     * 一个是当前的众数 x
      * @param nums
      * @return
      */
     public int majorityElement(int[] nums) {
-        // 假设第一个为众数
-        int votes = 0, x = 0;
-        for(int i = 1; i < nums.length; i++) {
-            // 如果票数为 0 ，则当前数字为众数
-            if(votes == 0) x = nums[i];
-            if(nums[i] == x) {
-                votes++;
+        int vote = 1, x = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (vote == 0) x = nums[i];
+            if (nums[i] == x) {
+                // 相同的数，票数 + 1
+                vote++;
             }else {
-                votes--;
+                // 不同的数，票数 - 1
+                vote--;
             }
         }
         return x;
