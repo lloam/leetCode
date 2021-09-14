b2cbc5b5c6d1e6b9a1fd3c9173409ff23dcb1050
lloam <2196143404@qq.com>
2021-09-14 23:38:56 +0800

    :art:剑指offer46把数字翻译成字符串

--- Sword_finger_offer/src/main/java/com/mao/剑指offer46把数字翻译成字符串/Solution.java
+++ Sword_finger_offer/src/main/java/com/mao/剑指offer46把数字翻译成字符串/Solution.java
@@ -11,34 +11,74 @@ package com.mao.剑指offer46把数字翻译成字符串;
  */
 public class Solution {
 
+//    /**
+//     * X1X2X3......Xi-2Xi-1Xi...Xn
+//     * 如果要求第 i 个数字以前的翻译方法的话，假设第 i 个为 dp[i]，就要看 i-1 与 i 能不能一起翻译，
+//     * 如果可以，则 dp[i] = dp[i-2] + dp[i-1]
+//     * 如果不可以，只能单独翻译，则 dp[i] = dp[i-1]
+//     * @param num
+//     * @return
+//     */
+//    public int translateNum(int num) {
+//        // 先将 num 转换成字符串
+//        String str = String.valueOf(num);
+//        int a = 1, b = 1;
+//        for (int i = 2; i <= str.length(); i++) {
+//            // 如果  10 <= Xi-1Xi <= 25，则 Xi-1Xi 能一起翻译
+////            if(str.substring(i-2,i).compareTo("10") >= 0 && str.substring(i-2,i).compareTo("25") <= 0) {
+////                int c = a + b;
+////                b = a;
+////                a = c;
+////            }else {
+////                int c = a;
+////                b = a;
+////                a = c;
+////            }
+//            String temp = str.substring(i-2,i);
+//            int c = (temp.compareTo("10") >= 0 && temp.compareTo("25") <=0) ? a + b : a;
+//            b = a;
+//            a = c;
+//        }
+//        return a;
+//    }
+
     /**
-     * X1X2X3......Xi-2Xi-1Xi...Xn
-     * 如果要求第 i 个数字以前的翻译方法的话，假设第 i 个为 dp[i]，就要看 i-1 与 i 能不能一起翻译，
-     * 如果可以，则 dp[i] = dp[i-2] + dp[i-1]
-     * 如果不可以，只能单独翻译，则 dp[i] = dp[i-1]
+     * 动态规划
+     * 看最后两位数是否能一起翻译
      * @param num
      * @return
      */
     public int translateNum(int num) {
-        // 先将 num 转换成字符串
-        String str = String.valueOf(num);
-        int a = 1, b = 1;
-        for (int i = 2; i <= str.length(); i++) {
-            // 如果  10 <= Xi-1Xi <= 25，则 Xi-1Xi 能一起翻译
-//            if(str.substring(i-2,i).compareTo("10") >= 0 && str.substring(i-2,i).compareTo("25") <= 0) {
-//                int c = a + b;
-//                b = a;
-//                a = c;
-//            }else {
-//                int c = a;
-//                b = a;
-//                a = c;
-//            }
-            String temp = str.substring(i-2,i);
-            int c = (temp.compareTo("10") >= 0 && temp.compareTo("25") <=0) ? a + b : a;
+
+        // int a = 1, b = 1;
+        // String str = String.valueOf(num);
+//        for (int i = 2; i < str.length(); i++) {
+//            // Xi-1Xi
+//            String temp = str.substring(i-2, i);
+//            int c = (temp.compareTo("10") >= 0 && temp.compareTo("25") <= 0) ? a + b : a;
+//            b = a;
+//            a = c;
+//
+//        }
+//        // 此题的动态规划计算是对称的
+//        for (int i = str.length() - 2; i >= 0; i--) {
+//            String temp = str.substring(i, i+2);
+//            int c = (temp.compareTo("10") >= 0 && temp.compareTo("25") <= 0) ? a + b : a;
+//            b = a;
+//            a = c;
+//        }
+        // 使用数字求余，省去字符串的空间
+        int a = 1, b = 1, x, y = num % 10;
+        while (num != 0) {
+            num /= 10;
+            x = num % 10;
+            int temp = x * 10 + y;
+            int c = (temp >= 10 && temp <= 25) ? a + b : a;
             b = a;
             a = c;
+            y = num % 10;
         }
         return a;
+
     }
 }
